ab453adac78d69b4535688a05a866a0928b34033
Michał Lechowicz <ichal.lechowicz@gmail.com>
2025-03-07 15:50:58 +0100

    Add branches support to endpoint

--- src/main/java/pl/lechowicz/service/GitHubRepositoryService.java
+++ src/main/java/pl/lechowicz/service/GitHubRepositoryService.java
@@ -1,13 +1,16 @@
 package pl.lechowicz.service;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import pl.lechowicz.client.GitHubApiClient;
+import pl.lechowicz.model.BranchDTO;
 import pl.lechowicz.model.RepositoryDTO;
 
 import java.util.Collections;
+import java.util.function.Function;
 
 @ApplicationScoped
 public class GitHubRepositoryService {
@@ -19,10 +22,31 @@ public class GitHubRepositoryService {
         return gitHubApiClient.getRepositories(username)
                 .onItem().transformToMulti(repositories -> Multi.createFrom().iterable(repositories))
                 .filter(repository -> !repository.fork())
-                .onItem().transform(repository -> new RepositoryDTO(
-                        repository.name(),
-                        repository.owner().login(),
-                        Collections.emptyList()
+                .onItem().transformToUni(repository -> {
+                    String repoName = repository.name();
+                    String owner = repository.owner().login();
+
+                    Multi<BranchDTO> branches = getBranches(owner, repoName);
+
+                    return Uni.combine().all().unis(
+                            Uni.createFrom().item(repoName),
+                            Uni.createFrom().item(owner),
+                            branches.collect().asList()
+                    ).asTuple()
+                            .onItem().transform(tuple -> new RepositoryDTO(
+                                    tuple.getItem1(),
+                                    tuple.getItem2(),
+                                    tuple.getItem3()
+                            ));
+                }).merge();
+    }
+
+    private Multi<BranchDTO> getBranches(String username, String repoName) {
+        return gitHubApiClient.getBranches(username, repoName)
+                .onItem().transformToMulti(branches -> Multi.createFrom().iterable(branches))
+                .onItem().transform(branch -> new BranchDTO(
+                        branch.name(),
+                        branch.commit().sha()
                 ));
     }
 }
--- src/test/java/pl/lechowicz/controller/GitHubRepositoryControllerTest.java
+++ src/test/java/pl/lechowicz/controller/GitHubRepositoryControllerTest.java
@@ -18,25 +18,29 @@ public class GitHubRepositoryControllerTest {
                 .statusCode(200)
                 .body("size()", greaterThan(0))
                 .body("every { it.name != null }", is(true))
-                .body("every { it.owner == '" + userName + "' }", is(true));
-               // .body("every { it.branches.size() > 0 }", is(true))
-               // .body("every { it.branches.every { branch -> branch.name != null && branch.lastCommitSha != null } }", is(true))
-               // .body("find { it.name == 'repo1' }.branches", hasItems(
-//                        allOf(
-//                                hasEntry("name", "main"),
-//                                hasKey("lastCommitSha")
-//                        ),
-//                        allOf(
-//                                hasEntry("name", "develop"),
-//                                hasKey("lastCommitSha")
-//                        )
-//                ))
-//                .body("find { it.name == 'repo2' }.branches", hasItems(
-//                        allOf(
-//                                hasEntry("name", "master"),
-//                                hasKey("lastCommitSha")
-//                        )
-//                ));
+                .body("every { it.owner == '" + userName + "' }", is(true))
+                .body("every { it.branches.size() > 0 }", is(true))
+                .body("every { it.branches.every { branch -> branch.name != null && branch.last_commit_sha != null } }", is(true))
+                .body("find { it.name == 'git-consortium' }.branches", hasItems(
+                        allOf(
+                                hasEntry("name", "master"),
+                                hasKey("last_commit_sha")
+                        )
+                ))
+                .body("find { it.name == 'Hello-World' }.branches", hasItems(
+                        allOf(
+                                hasEntry("name", "master"),
+                                hasKey("lastCommitSha")
+                        ),
+                        allOf(
+                                hasEntry("name", "octocat-patch-1"),
+                                hasKey("last_commit_sha")
+                        ),
+                        allOf(
+                                hasEntry("name", "test"),
+                                hasKey("last_commit_sha")
+                        )
+                ));
     }
 
 }
