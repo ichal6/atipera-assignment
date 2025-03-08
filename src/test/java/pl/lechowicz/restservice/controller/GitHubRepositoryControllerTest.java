a7f1d1712c52cda96e457957de797abe65d72ec5
Michał Lechowicz <ichal.lechowicz@gmail.com>
2025-03-08 10:55:47 +0100

    Update testListRepositories to use specific user and adjust assertions

--- src/test/java/pl/lechowicz/restservice/controller/GitHubRepositoryControllerTest.java
+++ src/test/java/pl/lechowicz/restservice/controller/GitHubRepositoryControllerTest.java
@@ -7,38 +7,22 @@ import static org.hamcrest.Matchers.*;
 
 public class GitHubRepositoryControllerTest {
     @Test
-    public void testListRepositories() {
-        String userName = "octocat";
+    public void testListRepositoriesForIchal6User() {
+        // Arrange
+        String userName = "ichal6";
+        int maxNumberOfResultsPerPageForGitHubAPI = 100;
 
+        // Act and Assert
         given()
                 .when().get("/api/github/repositories/" + userName)
                 .then()
                 .statusCode(200)
-                .body("size()", greaterThan(0))
+                .body("size()", greaterThan(maxNumberOfResultsPerPageForGitHubAPI))
                 .body("every { it.name != null }", is(true))
                 .body("every { it.ownerLogin == '" + userName + "' }", is(true))
                 .body("every { it.branches.size() > 0 }", is(true))
-                .body("every { it.branches.every { branch -> branch.name != null && branch.lastCommitSha != null } }", is(true))
-                .body("find { it.name == 'git-consortium' }.branches", hasItems(
-                        allOf(
-                                hasEntry("name", "master"),
-                                hasKey("lastCommitSha")
-                        )
-                ))
-                .body("find { it.name == 'Hello-World' }.branches", hasItems(
-                        allOf(
-                                hasEntry("name", "master"),
-                                hasKey("lastCommitSha")
-                        ),
-                        allOf(
-                                hasEntry("name", "octocat-patch-1"),
-                                hasKey("lastCommitSha")
-                        ),
-                        allOf(
-                                hasEntry("name", "test"),
-                                hasKey("lastCommitSha")
-                        )
-                ));
+                .body("every { it.branches.every { branch -> branch.name != null && branch.lastCommitSha != null } }",
+                        is(true));
     }
 
 }
